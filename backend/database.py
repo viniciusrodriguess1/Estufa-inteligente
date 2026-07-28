274dec0e264dbedb5102a17d78467391c4e735ee
viniciusrodriguess1 <viniciusrodrigues1108@gmail.com>
2026-07-28 13:24:26 -0300

    Adiciona suporte a banco de dados PostgreSQL no Render com psycopg2

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,11 @@ import os
 from sqlmodel import create_engine, SQLModel, Session
 from sqlalchemy import event
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./pibic.db")
+db_url = os.getenv("DATABASE_URL", "sqlite:///./pibic.db")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+DATABASE_URL = db_url
 
 # Configurações do SQLite para permitir conexões de múltiplas threads
 connect_args = {}
